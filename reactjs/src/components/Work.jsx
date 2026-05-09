7a47147e0300958a385edd80eb3ea25194fc151e
Varun Tomar <varunrajput8077@gmail.com>
2026-05-09 09:06:06 +0530

    change the ui

--- reactjs/src/components/Work.jsx
+++ reactjs/src/components/Work.jsx
@@ -29,7 +29,7 @@ export default function Work() {
         <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
             <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
             <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
-            <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development.</p>
+            <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in full stack development.</p>
 
             <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
                 {work.map((work) => (
